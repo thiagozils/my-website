792e755d393cea5b26322e75ae738cc8d1d80092
Thiago Andrey Zils <zilsthiago@gmail.com>
2023-03-08 18:57:23 -0300

    altered branch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'thiagozils', // Usually your GitHub org/user name.
   projectName: 'my-website', // Usually your repo name.
-  deploymentBranch: 'main',
+  deploymentBranch: 'deployment',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
